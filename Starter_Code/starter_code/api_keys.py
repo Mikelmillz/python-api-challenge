3ff224dd8abf47b3ce3fbb21c79c32bd1c05fa8b
Mikelmillz <mikelmillz6@gmail.com>
2023-11-13 16:32:57 -0600

    updoading the hw
    Adding what I worked on to Github. Not sure if the API key will get encrypted from that pyc but if it doesn't then I will delete it from github.

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "5e236fbe298cc07102f49252a9c65094"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d3406cacc77a498eaa18ee10d068cb43"
